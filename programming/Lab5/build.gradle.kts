73f30f8971c8bd776a55b1c3ddd14a83c02e6c3a
Demid0 <demid04ka@mail.ru>
2023-02-19 15:23:39 +0300

    Lab5 init

---
+++ programming/Lab5/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.charleskorn.kaml:kaml:0.51.0")
+    implementation("com.google.dagger:dagger:2.45")
+    annotationProcessor("com.google.dagger:dagger-compiler:2.45")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
