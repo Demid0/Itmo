3c2681f5f26952e3c2269e1be9bcf47a6c8b7c3f
Demid0 <demid04ka@mail.ru>
2023-03-16 00:30:05 +0300

    feat: add ExecuteScript command

--- programming/Lab5/src/main/kotlin/Commands/ExecuteScript.kt
+++ programming/Lab5/src/main/kotlin/Commands/ExecuteScript.kt
@@ -1,24 +1,59 @@
 package Commands
 
 import Utils.Tank
+import java.io.BufferedReader
+import java.io.EOFException
 import java.io.File
+import java.io.IOException
 import java.io.InputStreamReader
+import java.io.PrintWriter
 
 class ExecuteScript: Command {
     override fun execute(tank: Tank, args: List<String>) {
         try {
             val file_name = args[1]
-            val file = File(file_name)
-            val reader = InputStreamReader(file.inputStream())
-            val file_text = reader.readLines()
-            for (string in file_text) {
-                val input = string.split(" ").toMutableList()
-                input.removeAll(setOf("", { input.size }))
-                tank.invoker.invoke(tank, input)
+            if (tank.scriptStack.contains(file_name)) {
+                System.err.println("No way! Recursion.")
+                return
             }
-            println("Script $file_name")
+
+            tank.writer.println("Executing $file_name")
+
+            tank.scriptStack.add(file_name)
+
+            val file = File(file_name)
+            val fileReader = BufferedReader(InputStreamReader(file.inputStream()))
+
+            val devNull = File("/dev/null")
+            val devNullPrinter = PrintWriter(devNull)
+
+            val previousTankReader = tank.reader
+            val previousTankWriter = tank.writer
+
+            tank.reader = fileReader
+            tank.writer = devNullPrinter
+
+            try {
+                while (true) {
+                    val input = tank.reader.readLine().split(" ").toMutableList()
+                    input.removeAll(setOf("", { input.size }))
+                    tank.invoker.invoke(tank, input)
+                }
+            } catch (e: NullPointerException) {
+                tank.scriptStack.remove(file_name)
+
+                tank.writer = previousTankWriter
+                tank.reader = previousTankReader
+
+                tank.writer.println("Script $file_name is done.")
+            }
         } catch (e: Exception) {
             System.err.println("File not found")
         }
     }
 }
+/***
+ * example
+ * /home/demid/Desktop/Itmo/programming/Lab5/abab
+ *
+ */
