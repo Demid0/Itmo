c26a0989004ecbc43c3179ffb2e9a7b8715646aa
Demid0 <demid04ka@mail.ru>
2023-04-21 02:53:21 +0300

    feat: if applied, will add Lab5's code as common module

--- programming/Lab6/common/src/main/kotlin/Main.kt
+++ programming/Lab6/common/src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import utils.*
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+import java.io.BufferedReader
+import java.io.PrintWriter
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+/***
+ * Точка входа в программу
+ */
+fun main(args: Array<String>) {
+    startKoin {
+        modules(koinModule)
+    }
+    val utilFabric = UtilFabric()
+    val app: App by utilFabric.inject()
+    val starter: Starter by utilFabric.inject()
+    val bufferedReaderManager: ReaderManager<BufferedReader> by utilFabric.inject()
+    val reader = bufferedReaderManager.get()
+    val printWriterManager: WriterManager<PrintWriter> by utilFabric.inject()
+    val writer = printWriterManager.get()
+
+    starter.downloadLastSystemCondition()
+    try {
+        app.run(reader, writer)
+    } catch (_: NullPointerException) {
+        System.err.println("Зачем вы Ctrl+D нажали?")
+    }
 }
---
+++ programming/Lab6/common/src/main/kotlin/commands/Show.kt
@@ -0,0 +1,23 @@
+package commands
+
+import commands.utils.Command
+import commands.utils.CommandType
+
+/***
+ * show : вывести в стандартный поток вывода все элементы коллекции в строковом представлении
+ * @author Demid0
+ * @since 1.0
+ */
+class Show: Command(CommandType.NO_ARG) {
+    override fun execute(args: Any?): String {
+        val collection = data.collection
+        return if (collection.isEmpty()) "Collection is empty :("
+        else {
+            var out = "Collection:\n"
+            for (element in collection) {
+                out += element.toString() + "\n"
+            }
+            out.dropLast(1)
+        }
+    }
+}
