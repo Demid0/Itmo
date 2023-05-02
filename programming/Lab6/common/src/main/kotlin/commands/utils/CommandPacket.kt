0b3ac1c1ccbcf1880b244de033c971f1766017d6
Demid0 <demid04ka@mail.ru>
2023-05-03 02:48:59 +0300

    ?: Idea's jokes

--- programming/Lab6/common/src/main/kotlin/commands/utils/CommandPacket.kt
+++ programming/Lab6/common/src/main/kotlin/commands/utils/CommandPacket.kt
@@ -4,4 +4,6 @@ import collectionObjectsClasses.Route
 import kotlinx.serialization.Serializable
 
 @Serializable
-class CommandPacket(var command: Command?, var singleArg: String?, var objectArg: Route?)
+class CommandPacket(var command: Command?, var singleArg: String?, var objectArg: Route?) {
+    constructor() : this(null, null, null)
+}
