c5ec0ffef7c599ddbbf2d8387d583820df27f590
Demid0 <demid04ka@mail.ru>
2023-05-03 02:50:57 +0300

    feat: if applied? will add ClientAssistant (this class will handle client queries (serialize, deserialize, execute, commands and return SystemCommand which client will execute))

---
+++ programming/Lab6/server/src/main/kotlin/ClientAssistant.kt
@@ -0,0 +1,21 @@
+import commands.utils.CommandPacket
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import serializationStrategies.utils.Serializator
+
+class ClientAssistant: KoinComponent {
+    val serializator: Serializator by inject()
+
+    fun handleMessage(message: String) : String {
+        val commandPacket = deserializeMessage(message)
+
+    }
+
+    fun serializeMessage(commandPacket: CommandPacket) : String {
+        return serializator.serialize(commandPacket)
+    }
+
+    fun deserializeMessage(message: String) : CommandPacket {
+        return serializator.deserialize(message, CommandPacket())
+    }
+}
