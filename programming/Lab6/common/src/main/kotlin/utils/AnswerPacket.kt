3a7868c38308ac44b46684346981dc52a355172c
Demid0 <demid04ka@mail.ru>
2023-05-03 07:29:14 +0300

    feat: if applied, will add constructor without parameters (затычка)

--- programming/Lab6/common/src/main/kotlin/utils/AnswerPacket.kt
+++ programming/Lab6/common/src/main/kotlin/utils/AnswerPacket.kt
@@ -2,4 +2,5 @@ package utils
 
 class AnswerPacket(var commandName: String = "print_to_client", var singleArg: String?, var commandType: CommandType?) {
     constructor(singleArg: String?) : this(singleArg = singleArg, commandType = null)
+    constructor() : this("")
 }
