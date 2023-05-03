f5c233d0555da4816eb242da49b8257375fc3d24
Demid0 <demid04ka@mail.ru>
2023-05-03 05:21:21 +0300

    feat: if applied, will add executing client commands on server

--- programming/Lab6/server/src/main/kotlin/ClientAssistant.kt
+++ programming/Lab6/server/src/main/kotlin/ClientAssistant.kt
@@ -1,14 +1,16 @@
 import utils.ArgumentPacket
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import utils.ClientCommandInvoker
 import utils.Serializator
 
 class ClientAssistant: KoinComponent {
     val serializator: Serializator by inject()
 
     fun executeQuery(message: String) : String {
-        val commandPacket = deserializeMessage(message)
-
+        val argumentPacket = deserializeMessage(message)
+        val clientCommandInvoker = ClientCommandInvoker()
+        val out = clientCommandInvoker.invoke(argumentPacket)
     }
 
     fun serializeMessage(argumentPacket: ArgumentPacket) : String {
