6c08cef2f4e8d6ec8f55463134574c790e23ce8c
Demid0 <demid04ka@mail.ru>
2023-05-03 05:00:36 +0300

    feat: if applied, will add argument count validating

--- programming/Lab6/client/src/main/kotlin/utils/CommandParser.kt
+++ programming/Lab6/client/src/main/kotlin/utils/CommandParser.kt
@@ -3,7 +3,8 @@ package commands.utils
 import collectionObjectsClasses.Coordinates
 import collectionObjectsClasses.Location
 import collectionObjectsClasses.Route
-import commands.*
+import utils.ArgumentPacket
+import utils.CommandType
 
 class CommandParser {
     private var commands: HashMap<String, CommandType> = HashMap()
@@ -32,18 +33,30 @@ class CommandParser {
         commands.put(name, command)
     }
 
-    fun parse(args: MutableList<String>): CommandPacket {
-        return if (args.isEmpty()) CommandPacket(null, null, null)
+    fun parse(args: MutableList<String>): ArgumentPacket {
+        return if (args.isEmpty()) ArgumentPacket(null, null, null)
         else {
             val commandName = args[0]
             val commandType = commands[commandName]
-            if (commandType == null) CommandPacket(null, null, null)
+            if (commandType == null) ArgumentPacket()
             else {
                 when(commandType) {
-                    CommandType.NO_ARG -> CommandPacket(commandName, null, null)
-                    CommandType.SINGLE_ARG -> CommandPacket(commandName, args[1], null)
-                    CommandType.OBJECT_ARG -> CommandPacket(commandName, null, Route("", Coordinates(null, null), null, Location(1, 1f, 1, ""), 4.0))
-                    CommandType.MIXED_ARG -> CommandPacket(commandName, args[1], Route("", Coordinates(null, null), null, Location(1, 1f, 1, ""), 4.0))
+                    CommandType.NO_ARG -> {
+                        if (args.size != 1) ArgumentPacket()
+                        else ArgumentPacket(commandName, null, null)
+                    }
+                    CommandType.SINGLE_ARG -> {
+                        if (args.size != 2) ArgumentPacket()
+                        else ArgumentPacket(commandName, args[1], null)
+                    }
+                    CommandType.OBJECT_ARG -> {
+                        if (args.size != 1) ArgumentPacket()
+                        else ArgumentPacket(commandName, null, Route("", Coordinates(null, null), null, Location(1, 1f, 1, ""), 4.0))
+                    }
+                    CommandType.MIXED_ARG -> {
+                        if (args.size != 2) ArgumentPacket()
+                        else ArgumentPacket(commandName, args[1], Route("", Coordinates(null, null), null, Location(1, 1f, 1, ""), 4.0))
+                    }
                 }
             }
         }
