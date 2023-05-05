b55fab2cca445f467a053a5724bf21a80c662b21
Demid0 <demid04ka@mail.ru>
2023-05-05 07:16:34 +0300

    feat: if applied, will make AnswerPacket serializable

--- programming/Lab6/common/src/main/kotlin/utils/AnswerPacket.kt
+++ programming/Lab6/common/src/main/kotlin/utils/AnswerPacket.kt
@@ -1,5 +1,8 @@
 package utils
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class AnswerPacket(var commandName: String = "print_to_client", var singleArg: String?, var commandType: CommandType?) {
     constructor(singleArg: String?) : this(singleArg = singleArg, commandType = null)
     constructor() : this("")
