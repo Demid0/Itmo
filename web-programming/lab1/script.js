807e6978fd809b5f4cd392157c7c5eb9db84147d
Demid <demid04ka@mail.ru>
2023-10-07 03:34:09 +0300

    refactor js

--- web-programming/lab1/script.js
+++ web-programming/lab1/script.js
@@ -2,34 +2,38 @@ const table = document.getElementById('result_table');
 function displayResult(data){
     const row = document.createElement('tr');
     row.innerHTML = `
-                        <td>${data.X}</td>
-                        <td>${data.Y}</td>
-                        <td>${data.R}</td>
-                        <td>${data.currentTime}</td>
-                        <td>${data.executionTime}</td>
-                        <td>${data.result}</td>`;
+            <td>${data.x}</td>
+            <td>${data.y}</td>
+            <td>${data.r}</td>
+            <td>${data.currentTime}</td>
+            <td>${data.executionTime}</td>
+            <td>${data.result}</td>`;
     table.appendChild(row);
 }
 function validateY() {
-    let y = document.querySelector('input[name="Y"]').value.replace(',','.');
-    return -5 <= y && y <= 5
+    let y = document.querySelector('input[name="y"]').value.replace(',','.');
+    return -5 <= y && y <= 5;
 }
 
 function validateR() {
-    let r = document.querySelector('input[name="R"]').value.replace(',','.');
-    return 2 <= r && r <= 5
+    let r = document.querySelector('input[name="r"]').value.replace(',','.');
+    return 2 <= r && r <= 5;
 }
 
 function validate() {
-    let y = validateY().valueOf()
-    let r = validateR().valueOf()
-    document.getElementById("button").disabled = !(y && r);
+    let y = validateY().valueOf();
+    let r = validateR().valueOf();
+    buttonIsActive((y && r));
+}
+
+function buttonIsActive(b) {
+    document.getElementById("button").disabled = !b;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    let form = document.getElementById('form')
+    let form = document.getElementById('form');
     form.addEventListener('submit', function (event) {
-        event.preventDefault()
+        event.preventDefault();
         let x = document.querySelector('input[name="X"]').value.replace(',', '.');
         let y = document.querySelector('input[name="Y"]').value.replace(',', '.');
         let r = document.querySelector('input[name="R"]').value.replace(',', '.');
@@ -40,14 +44,15 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => {
             if (response.ok) {
                 return response.json();
-            } else throw new Error(response.status.toString());
+            } else throw new Error(response.statusText);
         })
         .then(data => {
             displayResult(data);
             form.reset();
+            buttonIsActive(false);
         })
         .catch(error => {
             console.error('error: ', error);
-        })
-    })
-})
+        });
+    });
+});
