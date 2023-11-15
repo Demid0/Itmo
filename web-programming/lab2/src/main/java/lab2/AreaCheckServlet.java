3437d84669d14f61214609b67e9d43786aff3683
Demid <demid04ka@mail.ru>
2023-11-15 08:29:40 +0300

    web-lab2-refactor: remove unused imports

--- web-programming/lab2/src/main/java/lab2/AreaCheckServlet.java
+++ web-programming/lab2/src/main/java/lab2/AreaCheckServlet.java
@@ -4,15 +4,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.json.JSONObject;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Date;
-
-import static lab2.AreaFunctions.checkArea;
 
 public class AreaCheckServlet extends HttpServlet {
 
