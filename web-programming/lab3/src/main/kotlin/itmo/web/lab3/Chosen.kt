ceb566ac3f08690815a4500ed7bbc8619578be42
Demid <demid04ka@mail.ru>
2024-01-06 03:34:45 +0300

    [WEB] lab3 very shit

--- web-programming/lab3/src/main/kotlin/itmo/web/lab3/Chosen.kt
+++ web-programming/lab3/src/main/kotlin/itmo/web/lab3/Chosen.kt
@@ -7,7 +7,15 @@ import java.io.Serializable
 @Named
 @SessionScoped
 class Chosen : Serializable {
-    var x: Double = .0
-    var y: String = ""
-    var r: Double = .0
+    var x = .0
+    var y = ""
+    var r = 0
+
+    val xValues = List(9) { ((it - 4).toDouble() / 2) }
+    val rValues = List(5)  { it + 1 }
+
+    fun xIsValid() = !x.isNaN() && xValues.contains(x)
+    fun yIsValid() = y.isNotEmpty() && y.isNotBlank() && (-3 <= y.toDouble() && y.toDouble() <= 5)
+    fun rIsValid() = rValues.contains(r)
+
 }
